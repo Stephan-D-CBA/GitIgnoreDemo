b30fcad7de0d6f72b5e9c2a1875221ce9a7f67d3
Nikolaj <supernikolaj@hotmail.com>
2018-03-06 13:11:13 +0100

    w

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,5 +12,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello jhkdc!");
+        System.out.println("Hello world!");
+        System.out.println("Hello jhkdc!");
+        
     }
 }
